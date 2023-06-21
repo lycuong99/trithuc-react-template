3800d4f6c370aff93f5290b8daf699ee03308377
cuong <lycuong99@gmail.com>
2023-06-21 09:09:21 +0700

    set up pwa

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,6 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+
 import './App.css'
-import Header from '@/components/Header';
+
 import ThemePrimaryColor from './components/ThemePrimaryColor';
 import Settings from '@/components/settings';
 import RtlLayout from './components/RtlLayout';
--- vite.config.js
+++ vite.config.js
@@ -1,14 +1,67 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import * as path from 'path'
+import fs from "fs/promises";
+import { VitePWA } from "vite-plugin-pwa";
+
+const manifestForPlugin = {
+  registerType: "autoUpdate",
+  devOptions: {
+    enabled: true,
+  },
+  // add this to cache all the
+  // static assets in the public folder
+  includeAssets: ["**/*"],
+  // add this to cache all the imports
+  workbox: {
+    globPatterns: ["**/*"],
+  },
+
+  manifest: {
+    name: "Tri Thuc",
+    short_name: "Tri Thuc",
+    description: "Tri Thuc React Template",
+    icons: [
+      {
+        src: "/android-chrome-192x192.png",
+        sizes: "192x192",
+        type: "image/png",
+      },
+      {
+        src: "/android-chrome-512x512.png",
+        sizes: "512x512",
+        type: "image/png",
+      },
+      {
+        src: "/apple-touch-icon.png",
+        sizes: "180x180",
+        type: "image/png",
+        purpose: "apple touch icon",
+      },
+      {
+        src: "/maskable_icon.png",
+        sizes: "225x225",
+        type: "image/png",
+        purpose: "any maskable",
+      },
+    ],
+    theme_color: "#171717",
+    background_color: "#e8ebf2",
+    display: "standalone",
+    scope: "/",
+    start_url: "/",
+    orientation: "portrait",
+  },
+};
+
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), VitePWA(manifestForPlugin)],
   resolve: {
     alias: [{ find: '@', replacement: path.resolve(__dirname, 'src') }],
   },
   esbuild: {
-    loader: "tsx",
+    loader: "jsx",
     include: [
       // Business as usual for .jsx and .tsx files
       "src/**/*.jsx",
@@ -32,5 +85,20 @@ export default defineConfig({
       "node_modules/**/*.ts",
     ],
     exclude: [],
-  }
+  },
+  optimizeDeps: {
+    esbuildOptions: {
+      plugins: [
+        {
+          name: "load-js-files-as-jsx",
+          setup(build) {
+            build.onLoad({ filter: /src\\.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
+          },
+        },
+      ],
+    },
+  },
 })
